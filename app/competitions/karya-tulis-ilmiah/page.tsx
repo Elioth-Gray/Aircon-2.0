14d201bb8f7642098b5786a9035abcad96c80175
Elioth <dharsuthdra@gmail.com>
2024-12-10 06:23:07 +0700

    competition detail

---
+++ app/competitions/karya-tulis-ilmiah/page.tsx
@@ -0,0 +1,12 @@
+
+import Navbar from "@/app/components/Navbar"
+
+const karyatulisilmiah = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default karyatulisilmiah
