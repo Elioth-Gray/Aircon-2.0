7701936a42b1aeb738a1a5d1ea8f5fb34d9b33ee
Richo Febrian <richofal25@gmail.com>
2024-12-09 23:07:06 +0700

    fixing home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,10 @@
-import Link from "next/link";
 import Navbar from "./components/Navbar";
-import ReadMoreButton from "./components/ReadMoreButton";
-import Cards from "./components/Cards";
 
 export default function Home() {
   return (
     <>
       <div className="">
         <Navbar />
-        {/* <ReadMoreButton /> */}
-        {}
-        <div className="grid grid-cols-2 gap-2  mx-auto px-10 sm:px-6 lg:px-8  ">
-          <div className="flex flex-col gap-2">
-            <Cards
-              title="ABOUT US"
-              description="What is Airlangga Convention 2.0?"
-              image="/about-us.svg"
-            />
-            <Cards
-              title="ABOUT US"
-              description="What is Airlangga Convention 2.0?"
-              image="/about-us.svg"
-            />
-          </div>
-          <div>
-            <Cards
-              title="COMPETITION"
-              description="We provide a platform for high school students to explore interests and talents in both academic and non-academic competitions."
-              image="/competition.svg"
-            />
-          </div>
-        </div>
       </div>
     </>
   );
