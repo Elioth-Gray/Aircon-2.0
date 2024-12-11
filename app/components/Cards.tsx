58a9faf88677d321fe55066618cad37ca1568125
Elioth <dharsuthdra@gmail.com>
2024-12-11 18:24:14 +0700

    home page update

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -1,22 +1,47 @@
-import React from "react";
-import Image from "next/image";
 import ReadMoreButton from "./ReadMoreButton";
 
 export default function Cards({
   title,
   description,
   image,
+  imageSize = "100%", // Default image size
+  color = "#0A3981", // Default color
+  titleColor = "#FFFFF",
+  bodyColor = "#FFFFF",
 }: {
   title: string;
   description: string;
-  image: string;
+  image?: string;
+  imageSize?: string; // Optional image size parameter
+  color?: string; // Optional color parameter
+  titleColor?: string;
+  bodyColor?: string;
 }) {
   return (
-    <div className="px-4 sm:px-6 lg:px-8 bg-[#0A3981] rounded-3xl py-5  flex-col">
-      <div className="flex flex-col">
-        <img className="items-center" src={image} alt="" />
-        <p className="pt-7 text-wrap font-bold text-[#D4ECF8]">{title}</p>
-        <p className="text-white text-xs pt-[9px] pb-[22px]">{description}</p>
+    <div
+      className="px-4 sm:px-6 lg:px-8 rounded-3xl py-5 shadow-lg" // Tambahkan tanda kutip
+      style={{ backgroundColor: color }}
+    >
+      <div className="flex flex-col items-center">
+        {image && (
+          <img
+            className="flex flex-col items-center justify-center"
+            src={image}
+            alt=""
+            style={{ width: imageSize }}
+          />
+        )}
+        <p
+          className={`${
+            image ? "pt-4" : "pt-0"
+          } text-wrap font-bold uppercase self-start`}
+          style={{ color: titleColor }}
+        >
+          {title}
+        </p>
+        <p className="text-xs pb-3 pt-1" style={{ color: bodyColor }}>
+          {description}
+        </p>
         <ReadMoreButton />
       </div>
     </div>
