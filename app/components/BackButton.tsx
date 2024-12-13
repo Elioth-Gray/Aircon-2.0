7c4b8c6ca56b086b27c548639e2e14be4c0693ca
Elioth <dharsuthdra@gmail.com>
2024-12-13 16:08:09 +0700

    About Us Desktop

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -5,7 +5,7 @@ const BackButton = () => {
   return (
     <button className="bg-black text-white px-6 py-2 rounded-xl text-xs flex flex-row items-center gap-3">
           <ArrowLeft size={22} />
-          <p>Back to home</p>
+          <p className='lg:text-base'>Back to home</p>
     </button>
   )
 }
