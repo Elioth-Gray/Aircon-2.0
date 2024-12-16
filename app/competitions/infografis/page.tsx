6b8d367b3f47e53b1ac89987d1849f8fe854c574
Richo Febrian <richofal25@gmail.com>
2024-12-16 13:54:15 +0700

    Set zod

--- app/competitions/infografis/page.tsx
+++ app/competitions/infografis/page.tsx
@@ -17,82 +17,64 @@ const karyatulisilmiah = () => {
     <>
       <div className="mx-10 mt-5 lg:mx-28">
         <BackButton />
-        {/*  */}
         {/* Card */}
         <div className="w-full grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-10 lg:mt-3">
           <div className="flex flex-col justify-between items-start text-start gap-3 bg-[#1F519A] p-6 px-8 text-white rounded-xl relative overflow-clip w-full h-full mt-8 lg:col-span-1">
             <div className="p-3 rounded-full bg-[#D9D9D9]">
-              <BookOpenText size={35} className="text-black" />
+              <Newspaper size={35} className="text-black" />
             </div>
             <Image
-              src="/KTI.svg"
+              src="/Infografis.svg"
               width={200}
               height={200}
               alt="KTI"
-              className="absolute bottom-32 left-80"
+              className="absolute bottom-32 lg:left-80 left-56"
             ></Image>
             <h1 className="font-winter mt-5 text-2xl w-3/4 text-wrap">
               Infografis
             </h1>
             <div>
               <p>Biaya Pendaftaran:</p>
-              <p>GRATIS (Tahap Awal)</p>
+              <p>
+                Rp40.000,00 /orang (Gelombang 1) <br /> Rp50.000,00 /orang
+                (Gelombang 2)
+              </p>
             </div>
           </div>
-          {/*  */}
           <div className="lg:w-full items-center lg:col-span-2">
             <h1 className="font-winter text-6 w-full text-4xl lg:mt-4">
               Infografis
             </h1>
-            {/*  */}
             {/* Sub Title */}
             <div className="w-full mt-5 flex flex-col gap-3">
               <div className="w-full flex flex-row justify-start items-center gap-2">
                 <CalendarDots size={28} />
-                <p className="text-sm font-semibold">
-                  16 Desember 2024 - 22 Januari 2025
-                </p>
+                <p className="text-sm">16 Desember 2024 - 25 Januari 2025</p>
               </div>
               <div className="w-full flex flex-row justify-start items-center gap-2">
                 <PhoneCall size={28} />
-                <p className="text-sm font-semibold">
+                <p className="text-sm">
                   <a
-                    href="https://wa.me/6281996716532"
+                    href="https://wa.me/6285706127776"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    CP: +62 819-9671-6532 (Fajri)
+                    CP: +62 857-0612-7776 (Echa)
                   </a>
                 </p>
               </div>
             </div>
-            {/*  */}
             {/* Tombol Daftar */}
             <button
               className="px-5 py-2 border border-white bg-black rounded-lg shadow-lg text-white mt-5 font-bold"
               onClick={() =>
-                router.push(
-                  "/competitions/karya-tulis-ilmiah/pendaftaran-tahap-awal"
-                )
+                router.push("/competitions/infografis/pendaftaran")
               }
             >
-              Daftar Tahap Awal
-            </button>{" "}
-            <br />
-            <button
-              className="px-5 py-2 border border-white bg-black rounded-lg shadow-lg text-white mt-5 font-bold"
-              onClick={() =>
-                router.push(
-                  "/competitions/karya-tulis-ilmiah/pendaftaran-tahap-lanjut"
-                )
-              }
-            >
-              Daftar Tahap Selanjutnya
+              Daftar Sekarang
             </button>{" "}
           </div>
         </div>
-
-        {/*  */}
         {/* Tema */}
         <div className="flex flex-col lg:flex-row mt-5 gap-5 lg:mt-10">
           <div className="w-full lg:w-1/2">
@@ -124,7 +106,6 @@ const karyatulisilmiah = () => {
             </div>
           </div>
         </div>
-          {/*  */}
         {/* Ketentuan Peserta */}
         <div className="w-full flex flex-col mt-5 gap-1">
           <h1 className="text-2xl font-winter uppercase">Ketentuan Peserta:</h1>
@@ -246,7 +227,6 @@ const karyatulisilmiah = () => {
             </ol>
           </div>
         </div>
-        {/*  */}
         {/* Ketentuan Poster */}
         <div className="w-full flex flex-col mt-5 gap-1">
           <h1 className="text-2xl font-winter uppercase">
@@ -366,16 +346,15 @@ const karyatulisilmiah = () => {
       </div>
       {/* Click to Download Full Guide Book */}
       <div className="w-full flex flex-row justify-between items-center text-center mt-8 mb-2">
-        <div className="w-1/3 py-[0.05rem] bg-black"></div>
+        <div className="w-1/3 py-[0.05rem] bg-black lg:w-full"></div>
         <a
           href="https://drive.google.com/drive/folders/1NqIQwkzro_ga1eRvAiQqKyVlCbKIsZn9?usp=drive_link"
           className="w-1/3 text-sm font-bold"
         >
           Click to Download Full Guide Book
         </a>
-        <div className="w-1/3 py-[0.05rem] bg-black"></div>
+        <div className="w-1/3 py-[0.05rem] bg-black lg:w-full"></div>
       </div>
-      {/*  */}
       {/* Pengumuman Lolos Tahap Abstrak */}
       <div className="mx-10 mt-8 mb-2">
         <div className="w-full flex flex-col justify-center items-center gap-5">
