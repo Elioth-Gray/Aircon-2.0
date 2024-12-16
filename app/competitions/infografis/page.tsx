46615e02997e4668caeb07024f39c2b4dbd35e7b
Elioth <dharsuthdra@gmail.com>
2024-12-16 10:43:17 +0700

    Competitions Fix

--- app/competitions/infografis/page.tsx
+++ app/competitions/infografis/page.tsx
@@ -15,96 +15,116 @@ const karyatulisilmiah = () => {
 
   return (
     <>
-      <div className="mx-10 mt-5">
+      <div className="mx-10 mt-5 lg:mx-28">
         <BackButton />
         {/*  */}
         {/* Card */}
-        <div className="flex flex-col justify-between items-start text-start gap-3 bg-[#1F519A] p-6 px-8 text-white rounded-xl relative overflow-hidden w-full h-auto mt-8 hover:bg-[#E38F49]">
-          <div className="p-3 rounded-full bg-[#D9D9D9]">
-            <Newspaper size={35} className="text-black" />
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-10 lg:mt-3">
+          <div className="flex flex-col justify-between items-start text-start gap-3 bg-[#1F519A] p-6 px-8 text-white rounded-xl relative overflow-clip w-full h-full mt-8 lg:col-span-1">
+            <div className="p-3 rounded-full bg-[#D9D9D9]">
+              <BookOpenText size={35} className="text-black" />
+            </div>
+            <Image
+              src="/KTI.svg"
+              width={200}
+              height={200}
+              alt="KTI"
+              className="absolute bottom-32 left-80"
+            ></Image>
+            <h1 className="font-winter mt-5 text-2xl w-3/4 text-wrap">
+              Infografis
+            </h1>
+            <div>
+              <p>Biaya Pendaftaran:</p>
+              <p>GRATIS (Tahap Awal)</p>
+            </div>
           </div>
-          <Image
-            src="/KTI.svg"
-            width={200}
-            height={200}
-            alt="KTI"
-            className="absolute bottom-28 left-56"
-          ></Image>
-          <h1 className="font-winter mt-5 text-2xl w-3/4 text-wrap">
-            Infografis
-          </h1>
-          <div>
-            <p>Biaya Pendaftaran:</p>
-            <p>
-              Rp40.000,00 /orang (Gelombang 1) <br /> Rp50.000,00 /orang
-              (Gelombang 2)
-            </p>
+          {/*  */}
+          <div className="lg:w-full items-center lg:col-span-2">
+            <h1 className="font-winter text-6 w-full text-4xl lg:mt-4">
+              Infografis
+            </h1>
+            {/*  */}
+            {/* Sub Title */}
+            <div className="w-full mt-5 flex flex-col gap-3">
+              <div className="w-full flex flex-row justify-start items-center gap-2">
+                <CalendarDots size={28} />
+                <p className="text-sm font-semibold">
+                  16 Desember 2024 - 22 Januari 2025
+                </p>
+              </div>
+              <div className="w-full flex flex-row justify-start items-center gap-2">
+                <PhoneCall size={28} />
+                <p className="text-sm font-semibold">
+                  <a
+                    href="https://wa.me/6281996716532"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    CP: +62 819-9671-6532 (Fajri)
+                  </a>
+                </p>
+              </div>
+            </div>
+            {/*  */}
+            {/* Tombol Daftar */}
+            <button
+              className="px-5 py-2 border border-white bg-black rounded-lg shadow-lg text-white mt-5 font-bold"
+              onClick={() =>
+                router.push(
+                  "/competitions/karya-tulis-ilmiah/pendaftaran-tahap-awal"
+                )
+              }
+            >
+              Daftar Tahap Awal
+            </button>{" "}
+            <br />
+            <button
+              className="px-5 py-2 border border-white bg-black rounded-lg shadow-lg text-white mt-5 font-bold"
+              onClick={() =>
+                router.push(
+                  "/competitions/karya-tulis-ilmiah/pendaftaran-tahap-lanjut"
+                )
+              }
+            >
+              Daftar Tahap Selanjutnya
+            </button>{" "}
           </div>
         </div>
-        {/*  */}
-        {/* Tombol Daftar */}
-        <button
-          className="px-5 py-2 border border-white bg-black rounded-lg shadow-lg text-white mt-5 font-bold"
-          onClick={() => router.push("/competitions/infografis/pendaftaran")}
-        >
-          Daftar Sekarang
-        </button>{" "}
-        {/*  */}
-        {/* Sub Title */}
-        <div className="w-full mt-5 flex flex-col gap-3">
-          <div className="w-full flex flex-row justify-start items-center gap-2">
-            <CalendarDots size={28} />
-            <p className="text-sm font-semibold">
-              16 Desember 2024 - 25 Januari 2025
-            </p>
-          </div>
-          <div className="w-full flex flex-row justify-start items-center gap-2">
-            <PhoneCall size={28} />
-            <p className="text-sm font-semibold">
-              <a
-                href="https://wa.me/6285706127776"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                CP: +62 857-0612-7776 (Echa)
-              </a>
-            </p>
-          </div>
-        </div>
+
         {/*  */}
         {/* Tema */}
-        <div className="w-full mt-5">
-          <h1 className="text-2xl font-winter w-full text-wrap uppercase">
-            Tema:
-          </h1>
-          <p className="text-wrap">
-            “Sinergi Generasi Post-Millenial dan Teknologi dalam Menjawab
-            Tantangan Menuju Indonesia Emas 2045”
-          </p>
-        </div>
-        {/*  */}
-        {/* Sub Tema */}
-        <div className="w-full flex flex-row justify-between mt-5 ">
-          <div className="w-1/2 m-1">
-            <h1 className="text-2xl font-winter w-full text-wrap uppercase">
-              Sub Tema:
-            </h1>
-            <p className="text-wrap">Lingkup Sains & Teknologi:</p>
-            <p className="text-wrap">1. Lingkungan</p>
-            <p className="text-wrap">2. Kesehatan</p>
-            <p className="text-wrap">3. Teknologi</p>
+        <div className="flex flex-col lg:flex-row mt-5 gap-5 lg:mt-10">
+          <div className="w-full lg:w-1/2">
+            <h1 className="font-winter text-2xl uppercase">Tema</h1>
+            <p>
+              “Sinergi Generasi Post-Millenial dan Teknologi dalam Menjawab
+              Tantangan Menuju Indonesia Emas 2045”
+            </p>
           </div>
-          <div className="w-1/2 m-1">
-            <h1 className="text-2xl font-winter w-full text-wrap uppercase">
-              <br />
-            </h1>
-            <p className="text-wrap">Lingkup Sosial & Humaniora:</p>
-            <p className="text-wrap">1. Ekonomi</p>
-            <p className="text-wrap">2. Sosial Budaya</p>
-            <p className="text-wrap">3. Pendidikan</p>
+          <div className="w-full">
+            <h1 className="font-winter uppercase text-2xl"> sub tema</h1>
+            <div className="w-full flex flex-row justify-between lg:justify-start lg:gap-5">
+              <div>
+                <p>Lingkup Sains dan Teknologi:</p>
+                <ol className="list-decimal ml-4">
+                  <li>Lingkungan</li>
+                  <li>Kesehatan</li>
+                  <li>Teknologi</li>
+                </ol>
+              </div>
+              <div>
+                <p>Lingkup Sosial dan Humaniora:</p>
+                <ol className="list-decimal ml-4">
+                  <li>Ekonomi</li>
+                  <li>Sosial Budaya</li>
+                  <li>Pendidikan</li>
+                </ol>
+              </div>
+            </div>
           </div>
         </div>
-        {/*  */}
+          {/*  */}
         {/* Ketentuan Peserta */}
         <div className="w-full flex flex-col mt-5 gap-1">
           <h1 className="text-2xl font-winter uppercase">Ketentuan Peserta:</h1>
