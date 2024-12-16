54a34ae7bd073f645ce228a55d301df53d3624b3
Elioth <dharsuthdra@gmail.com>
2024-12-16 13:48:36 +0700

    form validation

--- app/competitions/karya-tulis-ilmiah/pendaftaran-tahap-awal/page.tsx
+++ app/competitions/karya-tulis-ilmiah/pendaftaran-tahap-awal/page.tsx
@@ -3,11 +3,26 @@
 import React from "react";
 import BackButton from "@/app/components/BackButton";
 import { useRouter } from "next/navigation";
+import { ktiSchemaAwal } from "@/app/utils/schema";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const CompetitionPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof ktiSchemaAwal>>({
+    resolver: zodResolver(ktiSchemaAwal),
+  });
+
   const router = useRouter();
 
-  const handleSubmit = () => {
+  const onSubmit = (data: z.infer<typeof ktiSchemaAwal>) => {
+
+    console.log(data)
+
     router.push(
       "/competitions/karya-tulis-ilmiah/pendaftaran-tahap-awal/reminder"
     );
@@ -37,23 +52,29 @@ const CompetitionPage = () => {
             </div>
 
             <div className="mt-5 w-full">
-              <form action="" className="flex flex-col gap-8">
+              <form action="" className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
                 <div className="w-full flex flex-row justify-between gap-2">
                   <div className="flex flex-col w-1/2 gap-1">
                     <label htmlFor="namaTim">Nama Tim*</label>
                     <input
                       type="text"
-                      name="namaTim"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:py-2"
+                      {...register("namaTim")}
                     />
+                    {errors.namaTim && (
+                      <p className="text-red-500 text-sm">{errors.namaTim.message}</p>
+                    )}
                   </div>
                   <div className="flex flex-col w-1/2 gap-1">
                     <label htmlFor="asalSekolah">Asal Sekolah*</label>
                     <input
                       type="text"
-                      name="asalSekolah"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:py-2"
+                      {...register("asalSekolah")}
                     />
+                    {errors.asalSekolah && (
+                      <p className="text-red-500 text-sm">{errors.asalSekolah.message}</p>
+                    )}
                   </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-2">
@@ -61,29 +82,36 @@ const CompetitionPage = () => {
                     <label htmlFor="nomorTelepon">Nomor Telepon*</label>
                     <input
                       type="text"
-                      name="nomorTelepon"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:py-2"
+                      {...register("nomorTelepon")}
                     />
+                    {errors.nomorTelepon && (
+                      <p className="text-red-500 text-sm">{errors.nomorTelepon.message}</p>
+                    )}
                   </div>
                   <div className="flex flex-col w-1/2 gap-1">
                     <label htmlFor="email">Email *</label>
                     <input
                       type="text"
-                      name="email"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:py-2"
+                      {...register("email")}
                     />
+                    {errors.email && (
+                      <p className="text-red-500 text-sm">{errors.email.message}</p>
+                    )}
                   </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-2">
                   <div className="flex flex-col w-full gap-1">
-                    <label htmlFor="anggota1">
-                      Nama Lengkap Anggota 1 (Ketua)*
-                    </label>
+                    <label htmlFor="anggota1">Nama Lengkap Anggota 1 (Ketua)*</label>
                     <input
                       type="text"
-                      name="anggota1"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:w-[99%] lg:py-2"
+                      {...register("anggota1")}
                     />
+                    {errors.anggota1 && (
+                      <p className="text-red-500 text-sm">{errors.anggota1.message}</p>
+                    )}
                   </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-2">
@@ -91,9 +119,12 @@ const CompetitionPage = () => {
                     <label htmlFor="anggota2">Nama Lengkap Anggota 2</label>
                     <input
                       type="text"
-                      name="anggota2"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:w-[99%] lg:py-2"
+                      {...register("anggota2")}
                     />
+                    {errors.anggota2 && (
+                      <p className="text-red-500 text-sm">{errors.anggota2.message}</p>
+                    )}
                   </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-2">
@@ -101,55 +132,45 @@ const CompetitionPage = () => {
                     <label htmlFor="anggota3">Nama Lengkap Anggota 3</label>
                     <input
                       type="text"
-                      name="anggota3"
                       className="px-2 py-1 border border-black rounded-lg w-[98%] lg:w-[99%] lg:py-2"
+                      {...register("anggota3")}
                     />
+                    {errors.anggota3 && (
+                      <p className="text-red-500 text-sm">{errors.anggota3.message}</p>
+                    )}
                   </div>
                 </div>
-                <div className="w-full flex flex-row justify-between gap-2">
+                {/* <div className="w-full flex flex-row justify-between gap-2">
                   <div className="flex flex-col w-full gap-1">
-                    <label htmlFor="ktpFile">
-                      Scan KTP/ Kartu Pelajar* <br /> Note: Jadikan satu file
-                    </label>
+                    <label htmlFor="ktpFile">Scan KTP/ Kartu Pelajar*</label>
                     <input
                       type="file"
-                      name="ktpFile"
                       className="px-4 py-8 border border-black rounded-lg w-[98%] bg-white lg:w-[99%]"
-                      accept=".pdf"
-                      onChange={(e) => {
-                        const file = e.target.files?.[0];
-                        if (file && file.size > 5 * 1024 * 1024) {
-                          alert("File size exceeds 5 MB");
-                          e.target.value = "";
-                        }
-                      }}
+                      {...register("ktpFile")}
                     />
+                    {errors.ktpFile && (
+                      <p className="text-red-500 text-sm">{errors.ktpFile.message}</p>
+                    )}
                   </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-2">
                   <div className="flex flex-col w-full gap-1">
-                    <label htmlFor="ktpFile">
+                    <label htmlFor="abstractFile">
                       File Abstrak (Format pdf, Size limit 5 MB)*
                     </label>
                     <input
                       type="file"
-                      name="abstrakFile"
                       className="px-4 py-8 border border-black rounded-lg w-[98%] bg-white lg:w-[99%]"
-                      accept=".pdf"
-                      onChange={(e) => {
-                        const file = e.target.files?.[0];
-                        if (file && file.size > 5 * 1024 * 1024) {
-                          alert("File size exceeds 5 MB");
-                          e.target.value = "";
-                        }
-                      }}
+                      {...register("abstrakFile")}
                     />
+                    {errors.abstrakFile && (
+                      <p className="text-red-500 text-sm">{errors.abstrakFile.message}</p>
+                    )}
                   </div>
-                </div>
+                </div> */}
                 <button
                   className="bg-black text-white px-8 py-3 rounded-lg flex flex-row justify-center items-center w-1/4"
                   type="submit"
-                  onClick={handleSubmit}
                 >
                   Submit
                 </button>
@@ -158,7 +179,7 @@ const CompetitionPage = () => {
           </div>
           <div className="w-1/2 flex-row h-auto hidden lg:flex items-center justify-between">
             <div className="h-full px-[0.10rem] bg-black py-1 rounded-xl"></div>
-            <div className="font-winter uppercase text-8xl">
+            <div className="font-winter uppercase text-8xl ml-10">
               <h1
                 className="text-[#D4ECF8]"
                 style={{
@@ -167,14 +188,16 @@ const CompetitionPage = () => {
               >
                 #<span className="text-[#F9C84D]">Strive</span>
               </h1>
-              <h1 className="text-[#E38F49]"
+              <h1
+                className="text-[#E38F49]"
                 style={{
                   WebkitTextStroke: "1px #0A3981",
                 }}
               >
                 for
               </h1>
-              <h1 className="text-[#D4ECF8]"
+              <h1
+                className="text-[#D4ECF8]"
                 style={{
                   WebkitTextStroke: "1px #0A3981",
                 }}
--- app/supporter/page.tsx
+++ app/supporter/page.tsx
@@ -17,9 +17,9 @@ const SupporterPage = () => {
             <h1 className='uppercase text-2xl w-full text-white z-10 lg:hidden text-center'>Supporter</h1>
             <h1 className='uppercase w-full hidden lg:block  text-white z-10 text-center text-4xl'>Support Your Team</h1>
             <Image src="/supporter2.svg" width={300} height={90} alt='supporter' className='absolute top-2 left-60 lg:hidden'></Image>
-            <Image src="/supporter2.svg" width={300} height={90} alt='supporter' className='absolute top-2 right-20 lg:absolute lg:block'></Image>
+            <Image src="/supporter2.svg" width={300} height={90} alt='supporter' className='hidden top-2 right-20 lg:absolute lg:block'></Image>
         </div>
-        <div className='w-full mt-8 grid grid-cols-1 lg:grid-cols-3 gap-3'>
+        <div className='w-full mt-8 grid grid-cols-1 lg:grid-cols-3 gap-3   '>
         <SupporterCard school1="SMA 1 Ngawi" school2="SMA 5 Ngawi" gender="Boys" date="12-02-2023" location="Universitas Airlangga"></SupporterCard>
         <SupporterCard school1="SMA 1 Ngawi" school2="SMA 5 Ngawi" gender="Boys" date="12-02-2023" location="Universitas Airlangga"></SupporterCard>
         <SupporterCard school1="SMA 1 Ngawi" school2="SMA 5 Ngawi" gender="Boys" date="12-02-2023" location="Universitas Airlangga"></SupporterCard>
